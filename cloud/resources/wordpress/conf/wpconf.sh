3e66b7048f355ad003143e3c1fc8fcf43ac83d29
yamani5 <devforever99@gmail.com>
2022-04-13 20:54:43 +0000

    ep2-006

--- cloud/resources/wordpress/conf/wpconf.sh
+++ cloud/resources/wordpress/conf/wpconf.sh
@@ -4,15 +4,12 @@ if [ -f  "$FILE" ]; then
   sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/
           s/username_here/${WORDPRESS_DB_USER}/
           s/localhost/${WORDPRESS_DB_HOST}/
-          s/password_here/${WORDPRESS_DB_PASSWORD}/
-          ### Enable redis Cache Settings in wp-config.php ###
-          s/redis host/${WP_REDIS_HOST}/
-          s/salt/${WP_CACHE_KEY_SALT}/" $FILE
+          s/password_here/${WORDPRESS_DB_PASSWORD}" $FILE
 fi
 
-wp core install --path=/var/www/wordpress/ --allow-root --url=https://myamani.42.fr --title=inception --admin_user=myamani --admin_password=1337 --admin_email=myamani@email.com
-wp --path=/var/www/wordpress/ plugin activate redis-cache --allow-root
-wp --path=/var/www/wordpress/ redis update-dropin --allow-root
+# wp core install --path=/var/www/wordpress/ --allow-root --url=https://myamani.42.fr --title=inception --admin_user=myamani --admin_password=1337 --admin_email=myamani@email.com
+# wp --path=/var/www/wordpress/ plugin activate redis-cache --allow-root
+# wp --path=/var/www/wordpress/ redis update-dropin --allow-root
 ### start php-fpm in foreground ###
 ./usr/sbin/php-fpm7.3 -F -R
 
